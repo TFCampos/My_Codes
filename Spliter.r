2f9ec605943f858f6bc4371bc1e1a5bb71ca1e2c
Thiago Feitosa Campos <34857948+TFCampos@users.noreply.github.com>
2018-09-09 12:31:07 -0300

    Update Spliter.r

--- Spliter.r
+++ Spliter.r
@@ -12,7 +12,7 @@ Spliter<-function(TrainDataBase,TestDataBase,Vars,Target,IDs_Var){
                     control=list(maxdepth = 2))
     
     Splits<-Tree.Vars$splits[,4] %>% sort
-    if(is.null(Splits)) Splits<-TrainDataBase%>%select(Vars[i])%>%as.matrix()%>%median()
+    if(is.null(Splits)) Splits<-TrainDataBase%>%select(Vars[i])%>%as.matrix()%>%median(na.rm=T)
     Split_Out[i,]<-c(Vars[i],paste(Splits,collapse = " - "))
     if (length(Splits)==1) {
       conds<-paste(Vars[i],Splits,sep = '<')
